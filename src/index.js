c4938c39a323b2778eed0276024076ba11ee1311
Maya Mircheva <may4eto@hotmail.com>
2021-02-10 09:18:18 +0100

    added a hyphen

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class ButtonPad extends React.Component {
                 ["A", "B", "C", "D", "E", "F"],
                 ["G", "H", "I", "J", "K", "L", "M"],
                 ["N", "O", "P", "Q", "R", "S", "T"],
-                ["U", "V", "W", "X", "Y", "Z"]
+                ["U", "V", "W", "X", "Y", "Z", "-"]
             ],
             targetWord: props.targetWord,
             letterMap: {},
